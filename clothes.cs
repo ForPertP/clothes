c4de29c1fa3a1044e2e8b3a1fc12f3d46adcda55
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-12 04:02:47 +0900

    Create clothes.cs

---
+++ clothes.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(string[,] clothes) {
+        int answer = 1;
+        var counts = new Dictionary<string, int>();
+
+        int rowCount = clothes.GetLength(0);
+
+        for (int i = 0; i < rowCount; i++) 
+        {
+            string type = clothes[i, 1];
+            
+            if (counts.TryGetValue(type, out int count)) 
+            {
+                counts[type] = count + 1;
+            }
+            else 
+            {
+                counts[type] = 1;
+            }
+        }
+
+        foreach (int count in counts.Values) 
+        {
+            answer *= (count + 1);
+        }
+
+        return answer - 1;
+    }
+}
